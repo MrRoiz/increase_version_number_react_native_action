73c168b00d161594682137c27cb4e989bad97630
Nad GarDez <iranad@hotmail.com>
2022-09-29 17:41:56 -0400

    a bug

--- App.js
+++ App.js
@@ -86,7 +86,7 @@ const App: () => Node = () => {
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do neeeextt (some eeeee):
+            hey jud
           </Section>
           <LearnMoreLinks />
         </View>
